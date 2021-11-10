82ba412f8753a687f328b9bfa50fb6e130f84e09
HAoRAn-W <haoran2326@gmail.com>
2021-11-10 11:17:00 -0500

    add header file

---
+++ src/gyro.h
@@ -0,0 +1,15 @@
+#define WHO_AM_I 0x0F
+
+#define CTRL_REG_1 (0x20)
+#define CTRL_REG_2 (0x21)
+#define CTRL_REG_3 (0x22)
+#define CTRL_REG_4 (0x23)
+#define CTRL_REG_5 (0x24)
+
+#define STATUS_REG (0x27)
+#define OUT_X_L (0x28)
+#define OUT_X_H (0x29)
+#define OUT_Y_L (0x2A)
+#define OUT_Y_H (0x2B)
+#define OUT_Z_L (0x2C)
+#define OUT_Z_H (0x2D)
--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,18 @@
 */
 
 #include <mbed.h>
+#include <stm32f429xx.h>
+#include "gyro.h"
 
-#define OUT_X_L (0x28)
-#define OUT_X_H (0x29)
-#define OUT_Y_L (0x2A)
-#define OUT_Y_H (0x2B)
-#define OUT_Z_L (0x2C)
-#define OUT_Z_H (0x2D)
-#define CTRL_REG_1 (0x20)
-#define STATUS_REG (0x27)
 
 SPI gyroscope(PF_9, PF_8, PF_7); // mosi, miso, sclk
 DigitalOut cs(PC_1);
 
 char buffer[6]; // raw daat from gyro
 int16_t data[3]; // combined result from gyro
+int16_t x_data;
+int16_t y_data;
+int16_t z_data;
 
 float resolution = 1.0f;
 
@@ -35,21 +32,29 @@ void writeByte(uint8_t address, uint8_t data)
 void initiateGyro(){
   // set up gyroscope
   gyroscope.format(8, 3); // 8 bits per SPI frame; polarity 1, phase 0, clock frequency deafult 1 MHz
-  writeByte(CTRL_REG_1, 0b00001000);// write CTRL_REG_1, power on
+  // gyroscope.frequency(100);
+  writeByte(CTRL_REG_1, 0b00001111);// write CTRL_REG_1, power on
+  writeByte(CTRL_REG_2, 0b00000000);
+  writeByte(CTRL_REG_3, 0b00000000);
+  writeByte(CTRL_REG_4, 0b00000000);
+  writeByte(CTRL_REG_5, 0b00000000);
 }
 
 void getGyroValue(){
   cs = 0;
   gyroscope.write(OUT_X_L | 0x80 | 0x40);
-  for(int i = 0; i < 5; i++){
-    buffer[i] = gyroscope.write(0xff);
-  }
+  // for(int i = 0; i < 5; i++){
+  //   buffer[i] = gyroscope.write(0xff);
+  // }
+  x_data = gyroscope.write(0xff) << 8 | gyroscope.write(0xff);
+  y_data = gyroscope.write(0xff) << 8 | gyroscope.write(0xff);
+  z_data = gyroscope.write(0xff) << 8 | gyroscope.write(0xff);
   cs = 1;
 
 
-  data[0] = buffer[1] << 8 | buffer[0];
-  data[1] = buffer[3] << 8 | buffer[2];
-  data[2] = buffer[5] << 8 | buffer[4];
+  // data[0] = buffer[1] << 8 | buffer[0];
+  // data[1] = buffer[3] << 8 | buffer[2];
+  // data[2] = buffer[5] << 8 | buffer[4];
 }
 
 
@@ -60,11 +65,11 @@ int main()
   while (1)
   {
     getGyroValue();
-    printf("x=%d\r\n", data[0]);
-    printf("y=%d\r\n", data[1]);
-    printf("z=%d\r\n", data[2]);
+    printf("x=%d\r\n", x_data);
+    printf("y=%d\r\n", y_data);
+    printf("z=%d\r\n", z_data);
 
-    wait_us(100000);
+    //wait_us(100000);
   }
 }
 
